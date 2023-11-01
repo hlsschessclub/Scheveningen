f80358c8fa727f18e3d3dbd7e7b0a2dd9a738671
Muazzam Waheed <waheedmuazzam@gmail.com>
2023-11-01 16:01:45 -0400

    testing basic json to js conversion

---
+++ scripts/index.js
@@ -0,0 +1,24 @@
+let players = `[
+    {
+        "name":"",
+        "age":"",
+        "school":"",
+        "currentBoard":"" ,
+        "currentPoints":"",
+        "playersPlayed":["player1","player2","etc"]
+    },
+
+    {
+        "name":"",
+        "age":"",
+        "school":"",
+        "currentBoard":"" ,
+        "currentPoints":"",
+        "playersPlayed":["player1","player2","etc"]
+    }
+
+
+
+]`
+
+console.log(JSON.parse(companies)[0])
