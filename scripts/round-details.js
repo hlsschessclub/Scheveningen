37a069e7f7e5b64da8543234b3160f9f3a01d8e7
Siddh Patel <47033295+siddhp123@users.noreply.github.com>
2023-11-04 22:20:35 -0400

    made function callable

--- scripts/round-details.js
+++ scripts/round-details.js
@@ -1,13 +1,18 @@
 // Fetch all information from JSONBIN
-
-(async () => {
+async function fetchData() {
+  try {
     const res = await fetch('https://api.jsonbin.io/v3/b/6542b49d12a5d376599392e1/latest');
-    if(!res.ok) {
-      throw new Error(`'Request failed with status code: ${res.status}`);
+    if (!res.ok) {
+      throw new Error(`Request failed with status code: ${res.status}`);
     }
     const data = await res.json();
     console.log(data);
-  })();
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+// fetchData();
 
 // The information that needs to be accessed is the player info,
 // Should the matchups be compiled prior to the round start?
