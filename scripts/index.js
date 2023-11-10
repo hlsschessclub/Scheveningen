fafb2668c6b1e6afa4f6ada26a99a4d0d8a9d644
Siddh Patel <47033295+siddhp123@users.noreply.github.com>
2023-11-09 19:18:24 -0500

    Commented out test code

--- scripts/index.js
+++ scripts/index.js
@@ -50,32 +50,40 @@
 //   console.log(data);
 // })();
 
-function generateScheveningenPairings(teamA, teamB) {
-    const rounds = [];
-    const totalRounds = teamA.length;
-  
-    for (let round = 0; round < totalRounds; round++) {
-      const roundPairings = [];
-      for (let i = 0; i < teamA.length; i++) {
-        const playerA = teamA[i];
-        const playerB = teamB[(i + round) % teamA.length];
-        roundPairings.push([playerA, playerB]);
-      }
-      rounds.push(roundPairings);
-    }
-    return rounds;
-}
-  
-const teamA = [1, 2, 3, 4, 5];
-const teamB = [6, 7, 8, 9, 10];
+// function generateScheveningenPairings(teamA, teamB) {
+//   const schedule = [];
+//   const totalRounds = teamA.length;
 
-const pairings = generateScheveningenPairings(teamA, teamB);
+//   for (let round = 0; round < totalRounds; round++) {
+//     const roundGames = [];
 
-pairings.forEach((round, roundNumber) => {
-    console.log(`Round ${roundNumber + 1}:`);
-    round.forEach((pairing, index) => {
-        const [playerA, playerB] = pairing;
-        console.log(`Game ${index + 1}: Player ${playerA} (Team A) vs. Player ${playerB} (Team B)`);
-    });
-});
+//     for (let i = 0; i < teamA.length; i++) {
+//       const playerA = teamA[i];
+//       const playerB = teamB[(i + round) % teamA.length]; // Rotate teamB players in each round
+//       const colors = round % 2 === 0 ? ['White', 'Black'] : ['Black', 'White'];
+
+//       roundGames.push({
+//         white: { id: playerA },
+//         black: { id: playerB },
+//         result: -1, // You can update this with actual game results
+//       });
+//     }
+
+//     schedule.push({
+//       round: round + 1,
+//       games: roundGames,
+//     });
+//   }
+
+//   return schedule;
+// }
+
+// // Example usage:
+// const teamA = [1, 2, 3, 4, 5];
+// const teamB = [6, 7, 8, 9, 10];
+
+// const schedule = generateScheveningenPairings(teamA, teamB);
+
+// console.log(JSON.stringify({ schedule }, null, 2));
+
 
